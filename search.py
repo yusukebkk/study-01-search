03ba97a6d2fca2bfe268bd049ae1173fa20523d1
yusukekoide <y.koide@siteengine.co.jp>
2021-02-27 22:06:37 +0900

    CSVの読み込み機能を実装

--- search.py
+++ search.py
@@ -1,12 +1,19 @@
 
+import pandas as pd
 ### 検索ツールサンプル
 ### これをベースに課題の内容を追記してください
 
 # 検索ソース
 source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
 
+
 ### 検索ツール
 def search():
+    try:
+        source = get_csv_data("kimetsu.csv")
+        print("CSVデータからsourceを読み込みました")
+    except:
+        print("CSVデータが存在しないためデフォルトのsourceを利用します")
     word =input("鬼滅の登場人物の名前を入力してください >>> ")
 
     #wordがsource内に存在するか判定
@@ -17,6 +24,15 @@ def search():
         #見つからなかった場合リストに追加する
         source.append(word)
         print ("{}がリストに追加されました".format(word))
+        print (source)
+    
+#CSV読み込み機能
+def get_csv_data(file_name):
+    #CSVのname列を取得
+    df = pd.read_csv(file_name)
+    source = df['name'].tolist()
+    return source
+
 
 if __name__ == "__main__":
     search()
