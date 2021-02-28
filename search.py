254ab443ed8da35188ac51641298e36b7a02f310
yusukekoide <y.koide@siteengine.co.jp>
2021-03-01 02:36:57 +0900

    書き込み機能の追加

--- search.py
+++ search.py
@@ -5,13 +5,15 @@ import pandas as pd
 
 # 検索ソース
 source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
-
+file_name = "kimetsu.csv"
 
 ### 検索ツール
 def search():
+    source_type = "default"
     try:
-        source = get_csv_data("kimetsu.csv")
+        source = get_csv_data(file_name)
         print("CSVデータからsourceを読み込みました")
+        source_type = "csv"
     except:
         print("CSVデータが存在しないためデフォルトのsourceを利用します")
     word =input("鬼滅の登場人物の名前を入力してください >>> ")
@@ -22,9 +24,18 @@ def search():
     else:
         print("{}は見つかりませんでした".format(word))
         #見つからなかった場合リストに追加する
-        source.append(word)
-        print ("{}がリストに追加されました".format(word))
-        print (source)
+        if source_type == "default":
+            source.append(word)
+            print ("{}がデフォルトのリストに追加されました".format(word))
+            print (source)
+        else:
+            try:
+                result = update_csv_data(file_name,word)
+                print ("{}がCSVのリストに追加されました".format(word))
+                print (result)
+
+            except:
+                print("リストの追加に失敗しました")
     
 #CSV読み込み機能
 def get_csv_data(file_name):
@@ -33,6 +44,13 @@ def get_csv_data(file_name):
     source = df['name'].tolist()
     return source
 
+#CSV書き込み機能
+def update_csv_data(file_name,new_word):
+    df = pd.read_csv(file_name)
+    last_line = df.index.size
+    df.loc[last_line]= [new_word]
+    df.to_csv(file_name,index=False)
+    return df 
 
 if __name__ == "__main__":
     search()
